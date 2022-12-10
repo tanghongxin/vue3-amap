325df774feb5b3f59d7da0876a7f31e39d033707
tanghongxin <hongxin.tang@hotmail.com>
2022-12-10 16:47:03 +0800

    🚧 (NPM包拆分): 重命名 examples 为 example

--- build/config/example.js
+++ build/config/example.js
@@ -4,6 +4,8 @@ import { resolve } from 'path';
  * @type {() => import('vite').UserConfigExport}
  */
 export default () => ({
+  root: resolve(__dirname, '../../example'),
+  publicDir: resolve(__dirname, '../../example/public'),
   base: '/vue3-amap/',
   server: {
     https: true,
