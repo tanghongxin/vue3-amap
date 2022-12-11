47a6167127f056a59acfe3e95611be111dbb8853
tanghongxin <hongxin.tang@hotmail.com>
2022-12-11 19:39:44 +0800

    🔥 (NPM包拆分): 迁移 PositionWatcher 至 example 目录

--- example/src/components/global_components.js
+++ example/src/components/global_components.js
@@ -1,7 +1,9 @@
 import FenceView from './Fence/view/index.vue';
+import PositionWatcher from './PositionWatcher/index.vue';
 
 export default {
   install(app) {
     app.component(FenceView.name, FenceView);
+    app.component(PositionWatcher.name, PositionWatcher);
   },
 };
--- packages/index.js
+++ packages/index.js
@@ -4,7 +4,6 @@ import AMapGeolocation from './components/AMap/Geolocation/index.vue';
 import AMapMap from './components/AMap/Map/index.vue';
 import AMapMapType from './components/AMap/MapType/index.vue';
 import AMapMarker from './components/AMap/Marker/index.vue';
-import AMapPositionWatcher from './components/AMap/PositionWatcher/index.vue';
 import AMapScale from './components/AMap/Scale/index.vue';
 import AMapSearch from './components/AMap/Search/index.vue';
 import AMapToolbar from './components/AMap/Toolbar/index.vue';
@@ -19,7 +18,6 @@ export default {
       AMapMap,
       AMapMapType,
       AMapMarker,
-      AMapPositionWatcher,
       AMapScale,
       AMapSearch,
       AMapToolbar,
