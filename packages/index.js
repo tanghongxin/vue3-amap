d8018b44d40d83d7ea2e4061324c14f34c31b4cd
tanghongxin <hongxin.tang@hotmail.com>
2022-12-12 09:14:01 +0800

    ⚡️ (package): 全量导入导出全局组件

--- packages/index.js
+++ packages/index.js
@@ -1,42 +1,14 @@
-import AMapControlBar from './components/AMap/ControlBar/index';
-import AMapFitView from './components/AMap/FitView/index';
-import AMapGeolocation from './components/AMap/Geolocation/index';
-import AMapMap from './components/AMap/Map/index';
-import AMapMapType from './components/AMap/MapType/index';
-import AMapMarker from './components/AMap/Marker/index';
-import AMapScale from './components/AMap/Scale/index';
-import AMapSearch from './components/AMap/Search/index';
-import AMapToolbar from './components/AMap/Toolbar/index';
-import AMapVector from './components/AMap/Vector/index';
+import * as components from './components';
 
-export {
-  AMapControlBar,
-  AMapFitView,
-  AMapGeolocation,
-  AMapMap,
-  AMapMapType,
-  AMapMarker,
-  AMapScale,
-  AMapSearch,
-  AMapToolbar,
-  AMapVector,
-};
+export * from './components';
 
 export default {
   install(app, options) {
-    [
-      AMapControlBar,
-      AMapFitView,
-      AMapGeolocation,
-      AMapMap,
-      AMapMapType,
-      AMapMarker,
-      AMapScale,
-      AMapSearch,
-      AMapToolbar,
-      AMapVector,
-    ].forEach((Component) => {
-      app.use(Component, options);
+    Object.keys(components).forEach((key) => {
+      const component = components[key];
+      if (component.install) {
+        app.use(component, options);
+      }
     });
   },
 };
