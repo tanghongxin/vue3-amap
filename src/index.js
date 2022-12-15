b7636deaeff34757b78519f361be2773ae6d7213
tanghongxin <hongxin.tang@hotmail.com>
2022-12-15 20:40:30 +0800

    🔥components 导出迁移到单独文件，便于后期 TS 迁移

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
-import AMapControlBar from '../components/ControlBar/index';
-import AMapFitView from '../components/FitView/index';
-import AMapGeolocation from '../components/Geolocation/index';
-import AMapMap from '../components/Map/index';
-import AMapMapType from '../components/MapType/index';
-import AMapMarker from '../components/Marker/index';
-import AMapScale from '../components/Scale/index';
-import AMapSearch from '../components/Search/index';
-import AMapToolbar from '../components/Toolbar/index';
-import AMapVector from '../components/Vector/index';
+import * as components from '../components';
 
-const components = {
-  AMapControlBar,
-  AMapFitView,
-  AMapGeolocation,
-  AMapMap,
-  AMapMapType,
-  AMapMarker,
-  AMapScale,
-  AMapSearch,
-  AMapToolbar,
-  AMapVector,
-};
+export * from '../components';
 
 const install = function install(app, options) {
   Object.keys(components).forEach((key) => {
