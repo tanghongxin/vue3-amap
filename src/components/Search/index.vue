c0ad3a53f7cd83e0b69c4fb9963e4c528d94ed5a
tanghongxin <hongxin.tang@hotmail.com>
2024-04-12 16:45:03 +0800

    🐛 外部包名迁移

--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -12,7 +12,7 @@ import {
   defineComponent, onBeforeUnmount, onMounted,
 } from 'vue';
 import type { PropType } from 'vue';
-import { uuid } from '@tanghongxin/utils';
+import { uuid } from '@rthx/utils';
 import { handleError } from '~/core/error';
 import { useInjectMap } from '~/components/Map/composable';
 
