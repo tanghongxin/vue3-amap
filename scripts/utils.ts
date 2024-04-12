07db898031c08bca51213252d0a46fdc0e6aaa95
tanghongxin <hongxin.tang@hotmail.com>
2024-04-13 00:22:50 +0800

    👷 引入 release-it

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,11 +1,4 @@
-import { execSync } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
-
-export function run(command: string): string {
-  const output = execSync(command, { stdio: 'pipe' }).toString().trim();
-  console.log(output);
-  return output;
-}
 
 export function readJson(filePath: string): any {
   const fileContent = readFileSync(filePath, 'utf8');
